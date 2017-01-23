340d8645cfc50cab7ddac1ee1f905a982d1370ac
chrissy <chris@genius.com>
2017-01-23 14:35:31 -0600

    locking the viewport during transition. gonna test this with more images tho as most browsers may already do it?

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
   var leftTrigger = document.getElementById('portfolio-toggle-button');
   var slidingSections = document.querySelectorAll('.sliding-site-section');
+  var secondSection = document.querySelector('.sliding-site-section.second');
 
   leftTrigger.addEventListener('click', function(){
-    slidingSections.forEach(element => element.classList.toggle('showing'))
+    slidingSections.forEach(function(element) {
+      element.classList.toggle('showing');
+      document.body.classList.add('viewport-locked')
+      setTimeout(() => document.body.classList.remove('viewport-locked'), 500);
+    });
   });
 });
