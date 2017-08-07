93f52dfd3373fe2e27aa841a96cc9e4af15e5f5f
chrissy <chris@genius.com>
2017-08-07 12:12:59 -0700

    update images section and add button labels

--- script/minify_images.js
+++ script/minify_images.js
@@ -1,8 +1,9 @@
 const imagemin = require('imagemin');
 const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
 
-imagemin(['static/images/*.jpg'], 'static/images/compressed', {
-    plugins: [imageminMozjpeg({targa: true})]
+imagemin(['static/images/*.{jpg,png}'], 'static/compressed_images', {
+    plugins: [imageminMozjpeg(), imageminPngquant({quality: '65-80'})]
 }).then(files => {
     console.log(files);
 });
