383f0d41fb9328c9d4232942df7fc588f7fb2090
chrissy <chris@genius.com>
2017-09-11 18:53:12 -0700

    refactor 2

--- static/app.js
+++ static/app.js
@@ -3,28 +3,27 @@ import {WebGLRenderer, Scene, PerspectiveCamera, MeshBasicMaterial, Mesh, PlaneG
 import guideJson from '../script/guide.json';
 import shuffle from 'array-shuffle';
 
-const drawTerrain = ({plane, image, elevations}) => {
-  plane.geometry.vertices.map((v, i) => Object.assign(v, {z: elevations[i] / 100}));
-  plane.geometry.verticesNeedUpdate = true;
+const drawTerrain = ({mesh, image, elevations}) => {
+  mesh.geometry.vertices.map((v, i) => Object.assign(v, {z: elevations[i] / 100}));
+  mesh.geometry.verticesNeedUpdate = true;
   new TextureLoader().load(image, (img) => {
-    plane.material.map = img;
-    plane.material.needsUpdate = true;
+    mesh.material.map = img;
+    mesh.material.needsUpdate = true;
   });
 }
 
-const initializeRenderer = ({canvas, image, elevations}) => {
-  const width = Math.sqrt(elevations.length);
+const initializeMesh = (canvas) => {
   const size = Math.min(Math.max(canvas.offsetWidth, 550), 700);
   const camera = new PerspectiveCamera(42, 1, 0.1, 1000);
   const renderer = new WebGLRenderer({canvas, alpha: true});
   const scene = new Scene({autoUpdate: false});
-  const geometry = new PlaneGeometry(200, 200, width - 1, width - 1);
+  const geometry = new PlaneGeometry(200, 200, 100 - 1, 100 - 1);
   const material = new MeshBasicMaterial();
-  const plane = new Mesh(geometry, material);
+  const mesh = new Mesh(geometry, material);
 
   const animate = (z = 3.75, x = 5.6) => {
-    plane.rotation.z = z;
-    plane.rotation.x = x;
+    mesh.rotation.z = z;
+    mesh.rotation.x = x;
     renderer.render(scene, camera);
     window.requestAnimationFrame(() => animate(z + 0.0005, x + 0.00005));
   }
@@ -33,11 +32,10 @@ const initializeRenderer = ({canvas, image, elevations}) => {
   renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1);
   renderer.setSize(size, size);
 
-  drawTerrain({plane, image, elevations});
   animate()
-  scene.add(plane);
+  scene.add(mesh);
 
-  return {canvas, plane};
+  return mesh;
 }
 
 const getSketch = (name, cb) => fetch("/dist/" + name + ".json").then(r => r.json());
@@ -62,29 +60,30 @@ document.addEventListener("DOMContentLoaded", function() {
   const control = document.getElementById("canvas-control")
   const toggleButtons = document.querySelectorAll('[show],[hide]');
   const sketchTitle = document.getElementById('sketch-title');
+  const mesh = initializeMesh(canvas);
 
-  const showNextSketch = (renderer) => {
+  const showNextSketch = (mesh) => {
     sketches[sketches.length - 1].then((response) => {
       if (guide[sketches.length]) {
         sketches.push(getSketch(guide[sketches.length].name));
       } else {
         sketches = sketches.slice(0, 1)
       }
-      drawTerrain({plane: renderer.plane, image: response.image, elevations: response.elevations});
+      drawTerrain({mesh, image: response.image, elevations: response.elevations});
       sketchTitle.innerText = guide[sketches.length - 1].humanName;
     });
   }
 
   const initializeSketchControls = (renderer) => {
     sketchTitle.innerText = guide[0].humanName;
-    [canvas, control].forEach(c => c.addEventListener('click', () => showNextSketch(renderer)));
+    [canvas, control].forEach(c => c.addEventListener('click', () => showNextSketch(mesh)));
   }
 
   let sketches = guide.slice(0,2).map(s => getSketch(s.name));
 
   sketches[0].then((response) => {
-    const renderer = initializeRenderer({canvas, image: response.image, elevations: response.elevations});
-    initializeSketchControls(renderer);
+    initializeSketchControls(mesh);
+    drawTerrain({mesh, image: response.image, elevations: response.elevations});
   });
 
   if (toggleButtons) toggleButtons.forEach((toggleButton) => {
